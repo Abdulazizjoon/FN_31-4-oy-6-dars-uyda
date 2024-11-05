5ecb54faf49274d47984c81a5a2d30966a0a29ac
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-05 14:17:11 +0500

    .

---
+++ app.js
@@ -0,0 +1,73 @@
+//1-masala
+// let num=+prompt('son kiriting')
+// function c(a) {
+//     if (a%2==0) {
+//         console.log(`${a} juft son`);
+//     } else {
+//         console.log(`${a} toq son`);
+//     }
+// }
+// let res = c(num)
+
+//2-masala
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+
+// function maxNumber(a, b) {
+//     let sum=0
+//     if (a>b) {
+//         sum+=a
+//     } else {
+//         sum+=b
+//     }
+//     return sum
+// }
+// let res = maxNumber(son1, son2)
+// console.log(res);
+
+//3-masala
+// let son = +prompt("son kiriting");
+
+// function musbat(a) {
+//   if (a>0) {
+//     console.log(`${a} manfiy son`);
+//   } else {
+//       console.log(`${a} musbat son`);
+//   }
+// }
+// musbat(son);
+
+//4-masala
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+
+// function yigindi(a, b) {
+//   return (let = a + b);
+// }
+// let res = yigindi(son1, son2);
+// console.log(res);
+
+
+//5-masala
+// let son = +prompt("son kiriting");
+
+// function kvadrat(a) {
+//   return a *2;
+// }
+// let res = kvadrat(son);
+// console.log(res);
+
+
+//6-masala
+//     let son1= +prompt('son')
+//     let son2 = +prompt('son')
+
+//     function bolish(a,b) {
+//         return a / b
+// }
+//     bolish(son1,son2)
+
+
+
+
+
