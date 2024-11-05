ee3c542ffa12ec4754c6ff15b3b537ea93d55137
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-11-05 20:08:49 +0500

    .

--- app.js
+++ app.js
@@ -47,7 +47,6 @@
 // let res = yigindi(son1, son2);
 // console.log(res);
 
-
 //5-masala
 // let son = +prompt("son kiriting");
 
@@ -57,7 +56,6 @@
 // let res = kvadrat(son);
 // console.log(res);
 
-
 //6-masala
 //     let son1= +prompt('son')
 //     let son2 = +prompt('son')
@@ -66,6 +64,183 @@
 //         return a / b
 // }
 //     bolish(son1,son2)
+
+//7-masala
+// let son=+prompt('son kiriting')
+// function typElement(a) {
+//     a>0?console.log(`${a} musbat son`):a==0?console.log(`${a} nol`):console.log(`${a} manfiy son`);
+// }
+// let res = typElement(son);
+
+//8-masala
+// let son = +prompt('son kiriting')
+// function factoriyal(a) {
+//     let sum=1
+//     for (let i = 1; i <=a; i++) {
+//            sum*=i
+//     }
+//     return sum
+// }
+// let res = factoriyal(son)
+// console.log(res);
+
+//9-masala
+// let son = +prompt('son kiriting')
+
+// function karrali(a) {
+//     if (a%10==0) {
+//         console.log(`${a} 10ga karrali`);
+//     } else {
+//         console.log(`${a} 10ga karrali emas`);
+//     }
+// }
+
+// karrali(son);
+
+//10-masala
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+
+// function arifmetik(a, b) {
+//   let sum = a + b;
+
+//    return sum / 2;
+// }
+// let res = arifmetik(son1, son2);
+
+// console.log(res);
+
+//11-masala
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+// let son3 = +prompt("son kiriting");
+
+// function maxNumber(a,b,c) {
+//     if (a>b) {
+//         console.log(`${a} eng katta son`);
+//     }else if (b>c) {
+//         console.log(`${b} eng katta son`);
+
+//     } else {
+//         console.log(`${c} eng katta son`);
+//     }
+// }
+// maxNumber(son1,son2,son3)
+
+// 12-masala
+// let son =+prompt('son kirting')
+// function retur(a) {
+//     let sum=0
+//     if (a%2==0) {
+//         sum+=a
+//     } else {
+//         sum+=a+1
+//     }
+//     return sum
+// }
+// let res = retur(son)
+// console.log(res);
+
+//13-masala
+// let son =prompt("son kirting");
+// function num(a) {
+//     let sum=0
+//     for (let i = 1; i <=a.length; i++) {
+//         sum+=i
+//     }
+//     return sum
+// }
+// let res=num(son)
+// console.log(res);
+
+//15-masala
+// let son = +prompt('son kiriting')
+// function bolen(a) {
+//     if (a>0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// bolen(son)
+
+//16-masala
+// let son1 = +prompt('son kiriting')
+// let son2 = +prompt('son kiriting')
+// function functin(a, b) {
+//     let sum=0
+//     for (let i = a; i <=b; i++) {
+//             sum+=i
+//     }
+//     return sum
+// }
+// let res = functin(son1, son2)
+// console.log(res);
+
+//20-masala
+// let son = +prompt("son kiriting");
+// function tub(a) {
+//     if (a / 1 == a && a / a == 1) {
+//       console.log(`${a} tub son`);
+//     } else {
+//         console.log(`${a} tub emas`);
+//   }
+// }
+// tub(son);
+
+
+//21-masala
+// let son1 = +prompt("son kiriting");
+// let son2 = +prompt("son kiriting");
+// function kvadrat(a,b) {
+//     let sum = a * a + b * b
+//     return sum
+// }
+// let res=kvadrat(son1, son2)
+// console.log(res);
+
+//22-masala
+// let son = +prompt('son kiriting')
+// let nev =+prompt('yangi son kiriting')
+// function num(a) {
+//     let sum;
+//     return sum=nev
+// }
+// let res=num(son)
+// console.log(res);
+
+//23-masala
+// let son = +prompt('son kiriting')
+// function kar(a) {
+//     let sum = a * a * a
+//     return sum
+// }
+// let res=kar(son)
+// console.log(res);
+
+
+//24-masala
+// let son=+prompt('son kiriting')
+// let err=[
+//     0,
+//     1,
+//     2,
+//     3,
+//     5,
+//     8,
+//     13
+// ]
+// function febinus(a,b) {
+//     for (let i = err; i <=a.length; i++) {
+//         if (i==b) {
+//             console.log('salom');
+            
+//         }
+//     }
+// }
+// let res=febinus(err,son)
+// console.log(res);
+
 
 
 
